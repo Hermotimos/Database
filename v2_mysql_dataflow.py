cf7cb95fd9f3e23336c80321fb35f8075ab16854
Lukasz <lukas.kozicki@gmail.com>
2019-03-24 10:23:15 +0100

    __init__ and __str__ defined

--- v2_mysql_dataflow.py
+++ v2_mysql_dataflow.py
@@ -1,36 +1,23 @@
-import os.path                          # todo delete when no more need
-from collections import Counter         # todo delete when no more need
 import mysql.connector
 
 
-class Title:
-    def __init__(self, name):
-        self.name = name
-
-
 class Database:
-    def __init__(self, sourcefile):
-        self.sourcefile = sourcefile
-        assert os.path.isfile(sourcefile), "No source file or invalid sourcefile for database provided!"
-        self.openedfile = open(file=sourcefile, mode='r')
-        self.db_dict = {}
+    def __init__(self, source_db):
+        self.source_db = source_db
+        self.eval_db = mysql.connector.connect(
+                                                host='localhost',
+                                                user='root',
+                                                passwd='forPythonuse//',
+                                                database=source_db
+                                                )
+        self.mycursor = self.eval_db.cursor()
 
     def __str__(self):
-        """ k.name - k is an instance of 'Title' class, so k.name accesses it's 'name' attr """
-        return '\n'.join("{}: {}".format(k.name, v) for (k, v) in self.into_dict().items())
+        self.mycursor.execute("SELECT * FROM movies_evaluations")
+        return '\n'.join("{} | {} | {}".format(eval_id, title, score) for eval_id, title, score in self.mycursor)
 
-    def into_dict(self):
-        """ Transform file into dict with possibly identical titles within unique instances of 'Title' class"""
-        for line in self.openedfile:
-            k, v = line.strip().split(': ')
-            self.db_dict[Title(k.strip('""'))] = int(v.strip())
-        return self.db_dict
 
-    def insert_eval(self, title, evaluation):
-        """Reopen file in append mode to move cursor to end of file, reopen in read mode to move cursor to beginning"""
-        self.openedfile = open(file=self.sourcefile, mode='a')
-        self.openedfile.write("\n\"{}\": {}".format(title, evaluation))
-        self.openedfile = open(file=self.sourcefile, mode='r')
+
 
     def get_evals(self, title):
         evals = ''
@@ -51,11 +38,14 @@ class Database:
         n = (k.name for k in self.into_dict().keys() if k.name == title)
         return Counter(n)[title]
 
+    def insert_eval(self, title, evaluation):
+        """Reopen file in append mode to move cursor to end of file, reopen in read mode to move cursor to beginning"""
+        self.openedfile = open(file=self.source_db, mode='a')
+        self.openedfile.write("\n\"{}\": {}".format(title, evaluation))
+        self.openedfile = open(file=self.source_db, mode='r')
 
-# c = Database()
-# # c.insert_eval("The sth", 5)
-# print(c)
-# print()
-# print(c.get_evals('The sth'))
-# print(c.cnt_evals("The sth"))
-# print(c.avg_evals('The Wire'))
+
+
+
+c = Database('evaluations')
+print(c)
