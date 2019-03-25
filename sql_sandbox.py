9eb471902823269bfb15be195aca38aa8d14abc0
Lukasz <lukas.kozicki@gmail.com>
2019-03-25 17:07:27 +0100

    file copied, obvious comments deleted

--- sql_sandbox.py
+++ sql_sandbox.py
@@ -1,21 +1,2 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(host='localhost', user='root', passwd='forPythonuse//', database='evaluations')
-mycursor = mydb.cursor()
-
-
-
-
-
-
-
-
-tuple = (1, 2, 3, 4, 5)
-
-
-x = str(tuple[0])
-print(x)
-
-y = ''.join("{} | {} | {}".format(str(tuple[0]), str(tuple[1]), str(tuple[2])))
-print(y)
-
+for _ in range(7):
+    print('Hello, World.')
--- start.py
+++
@@ -1,6 +0,0 @@
-from interaction import do_action, question
-
-is_start = True
-while is_start is True:
-    do_action()
-    is_start = question()
