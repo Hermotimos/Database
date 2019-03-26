6b9e15254a78c84f783b7106e9b376fcadb35802
Lukasz <lukas.kozicki@gmail.com>
2019-03-26 19:33:09 +0100

    .get_table() method for use in __str__ as well as in future for retrieving whole table (all data for chosen table)

--- db_class.py
+++ db_class.py
@@ -21,19 +21,32 @@ class MySQLDB:
                "password: *******\n" \
                "Tables: {}".format(self.database, self.host, self.user, self.list_tables)
 
+    def get_table(self, table):
+        printout = '\n\n{}\n'.format(table).upper()
+        printout += '--\t{:40}\t-----'.format('-----')
+        printout += '\nID\t{:40}\tSCORE\n'.format('TITLE')
+        printout += '--\t{:40}\t-----'.format('-----')
+        self.cursor.execute("SELECT * FROM {}".format(table))
+        for row in self.cursor:
+            printout += '\n{:}\t{:40}\t{}'.format(row[0], row[1], row[2])
+        return printout
+
     def __str__(self):
         printout = ''
         for table in self.list_tables:
-            printout += '\n\n{}\n'.format(table).upper()
-            printout += '--\t{:40}\t-----'.format('-----')
-            printout += '\nID\t{:40}\tSCORE\n'.format('TITLE')
-            printout += '--\t{:40}\t-----'.format('-----')
-            self.cursor.execute("SELECT * FROM {}".format(table))
-            for row in self.cursor:
-                printout += '\n{:}\t{:40}\t{}'.format(row[0], row[1], row[2])
+            printout += self.get_table(table)
         return printout
+
+    def query(self, columns='*', table='', where=''):
+        query = 'SELECT {} FROM {} '.format(columns, table)
+        if where != '':
+            query += 'WHERE {} '.format(where)
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        return result
 
 
 d = MySQLDB(host='localhost', user='root', database='evaluations')
-print(repr(d))
-print(d)
+# print('\nTest .query()', d.query('evaluation_id', 'movies_evaluations'))            # todo NOT YET DONE
+# print('\nTest .get_table()', d.get_table('tvseries_evaluations'))
+print('\nTest __str__', d)
