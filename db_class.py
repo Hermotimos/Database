886f3852a531f2ea9eb5bdca4028552f1ccf32eb
Lukasz <lukas.kozicki@gmail.com>
2019-03-26 15:29:31 +0100

    __str__ created but not ready

--- db_class.py
+++ db_class.py
@@ -8,21 +8,36 @@ class MySQLDB:
         self.password = password
         self.database = database
 
-        self.connection = mysql.connector.connect(
-                                                    host=host,
-                                                    user=user,
-                                                    passwd=password,
-                                                    database=database,
-                                                    )
+        self.connection = mysql.connector.connect(host=host, user=user, passwd=password, database=database)
         self.cursor = self.connection.cursor()
 
-    def __repr__(self):
         self.cursor.execute("SHOW TABLES")
+        self.list_tables = [t[0] for t in self.cursor]
+
+    def __repr__(self):
         return "MySQL Database: {}\n" \
                "host: {}\n" \
                "user: {}\n" \
                "password: *******\n" \
-               "Tables: {}".format(self.database, self.host, self.user, [t[0] for t in self.cursor])
+               "Tables: {}".format(self.database, self.host, self.user, self.list_tables)
+
+    def __str__(self):
+        def get_rows(cursor, table):
+            cursor.execute("SELECT * FROM {}".format(table))
+            return [row for row in cursor]
+
+        def db_tables_union(cursor):
+            printout =
+            for table in self.list_tables:
+                printout.append(table)
+                rows = get_rows(cursor, table)
+                for row in rows:
+                    printout.append(row)
+            return printout
+
+        lst = db_tables_union(self.cursor)
+
+        return str([row for row in lst])
 
 
 d = MySQLDB(host='localhost', user='root', database='evaluations')
