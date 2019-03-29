9b7bc7ccc8a0d1c04291f217ebd106d6771a8dbb
Lukasz <lukas.kozicki@gmail.com>
2019-03-29 13:35:28 +0100

    added comments and test (prints database)

--- create_database.py
+++ create_database.py
@@ -1,6 +1,9 @@
 import mysql.connector
 from random import random
 
+
+# CONNECT TO MySQL AND CREATE DATABASE
+
 password = input('Enter password to connect to database:\n')
 mydb = mysql.connector.connect(host='localhost', user='root', passwd=password)
 mycursor = mydb.cursor()
@@ -8,15 +11,14 @@ mycursor = mydb.cursor()
 mycursor.execute("DROP DATABASE IF EXISTS evaluations")
 mycursor.execute("CREATE DATABASE evaluations")
 mycursor.execute("SHOW DATABASES")
-
 for db in mycursor:
-    print(db[0])
+    print('database: ', db[0])
 
 mydb = mysql.connector.connect(host='localhost', user='root', passwd=password, database='evaluations')
 mycursor = mydb.cursor()
 
 
-"""CREATE TABLES"""
+# CREATE TABLES AFTER DROPPING THEM IF EXIST
 
 tables_to_create = ['movies_evaluations', 'tvseries_evaluations', 'pcgames_evaluations', 'boardgames_evaluations']
 
@@ -36,8 +38,7 @@ for table in mycursor:
     print('table: ', table[0])
 
 
-""" POPULATE TABLES WITH RANDOM NUMBER OF EVALUATIONS HAVING RANDOM EVALUATION SCORE"""
-
+# POPULATE TABLES WITH RANDOM NUMBER OF EVALUATIONS HAVING RANDOM EVALUATION SCORE
 
 def generate_evaluations(titles):
     """ Returns list of 2-element tuples (title, evaluation) """
@@ -103,17 +104,19 @@ random_evals_tvseries = generate_evaluations(tvseries_titles)
 random_evals_pcgames = generate_evaluations(pcgames_titles)
 random_evals_boardgames = generate_evaluations(boardgames_titles)
 
-insert_into_movies_evaluations = "INSERT INTO movies_evaluations (title, score)" \
-                   "VALUES (%s, %s)"
-insert_into_tvseries_evaluations = "INSERT INTO tvseries_evaluations (title, score)" \
-                   "VALUES (%s, %s)"
-insert_into_pcgames_evaluations = "INSERT INTO pcgames_evaluations (title, score)" \
-                   "VALUES (%s, %s)"
-insert_into_boardgames_evaluations = "INSERT INTO boardgames_evaluations (title, score)" \
-                   "VALUES (%s, %s)"
+insert_into_movies_evaluations = "INSERT INTO movies_evaluations (title, score) VALUES (%s, %s)"
+insert_into_tvseries_evaluations = "INSERT INTO tvseries_evaluations (title, score) VALUES (%s, %s)"
+insert_into_pcgames_evaluations = "INSERT INTO pcgames_evaluations (title, score) VALUES (%s, %s)"
+insert_into_boardgames_evaluations = "INSERT INTO boardgames_evaluations (title, score) VALUES (%s, %s)"
 
 mycursor.executemany(insert_into_movies_evaluations, random_evals_movies)
 mycursor.executemany(insert_into_tvseries_evaluations, random_evals_tvseries)
 mycursor.executemany(insert_into_pcgames_evaluations, random_evals_pcgames)
 mycursor.executemany(insert_into_boardgames_evaluations, random_evals_boardgames)
 mydb.commit()
+
+
+# TEST db:
+# from db_class import MySQLDB
+# db = MySQLDB(host='localhost', user='root', database='evaluations')
+# print(db)
